4ececf8ada551050c82c204d3a3ce4c8a2466a57
Arjun Bharadwaj <m.arjunbharadwaj@gmail.com>
2025-03-21 16:13:04 -0700

    2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import pymongo
-from pymongo import MongoClient 
+import pymongo                  #importing pymongo(struggled)
+from pymongo import MongoClient  # importing mongoclient from pymongo library
+
+cluster = MongoClient()             # linking the cluster  (local cluster because the atlas one is not working at all)
+db = cluster["Test"]        #creating a database in the cluster and naming it 
+Collection = db["TestC"]      #creating a collection inside the database and naming it
+
+## Cluster is a space in cloud, Database is set of collections in the cloud and collection is kindoff mini database containing posts. Posts are bascically information posted in form of post lol
+# Posts are in Json aka Dictionary Format {}
+
+
 
